03c9089a073f13518123dd30ff9b028da6a07ada
NartRamadan <kataz91@gmail.com>
2025-03-08 01:25:55 +0200

    הגדרת חיבור WIFI עם פורט

--- ES_Clinet/ClientApp.ino
+++ ES_Clinet/ClientApp.ino
@@ -1,3 +1,14 @@
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <WiFiUdp.h>
+#include <ESP8266WiFi.h>
+
+const char* SSID = "NartRamadan";
+const char* pswd = "19911991";
+
+WiFiClient client;
+int server_port = 443 ; 
+
 void setup() {
     Serial.begin(9600);
     wifi_Setup();
